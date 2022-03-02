920a2d40da6b66ca57f958f5887749cb46412afd
Danila Usachev <>
2022-03-02 21:02:47 +0300

    Implemented rendering result to string

--- src/main/kotlin/org/intellij/sdk/action/Renderers.kt
+++ src/main/kotlin/org/intellij/sdk/action/Renderers.kt
@@ -3,6 +3,7 @@ package org.intellij.sdk.action
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
+import kotlin.math.roundToInt
 
 /**
  * An object which can show results of [CodeOwnerFinderAction] to the user.
@@ -10,6 +11,22 @@ import com.intellij.openapi.ui.Messages
 interface CodeOwnerFinderRenderer {
     fun success(result: CodeOwnerResult)
     fun error(exception: CodeOwnerFinderException)
+}
+
+/**
+ * Selects [maxAuthors] best candidates,
+ * and renders them to string.
+ */
+private fun renderResultToTopNLines(result: CodeOwnerResult, maxAuthors: Int): String {
+    return result
+        .authorToKnowledgeLevel
+        .toList()
+        .sortedByDescending { it.second }
+        .take(maxAuthors)
+        .joinToString("\n") { (author, knowledgeLevel) ->
+            val percentage = (knowledgeLevel * 100).roundToInt()
+            "$author: knows $percentage%"
+        }
 }
 
 /**
@@ -21,11 +38,12 @@ class CodeOwnerFinderDialogRenderer(
 
     companion object {
         const val DIALOG_TITLE = "Code Owner Finder"
+        const val MAX_AUTHORS_IN_RESULT = 5
     }
 
     override fun success(result: CodeOwnerResult) = Messages.showMessageDialog(
         project,
-        result._temp,
+        renderResultToTopNLines(result, MAX_AUTHORS_IN_RESULT),
         DIALOG_TITLE,
         AllIcons.General.SuccessDialog,
     )
