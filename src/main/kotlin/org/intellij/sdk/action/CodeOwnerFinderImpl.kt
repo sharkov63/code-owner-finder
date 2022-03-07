5fd4496adcab77fb04c8e65de3663b1516b51560
Danila Usachev <>
2022-03-07 18:45:13 +0300

    Refactored LineKnowledgeAdder

--- src/main/kotlin/org/intellij/sdk/action/CodeOwnerFinderImpl.kt
+++ src/main/kotlin/org/intellij/sdk/action/CodeOwnerFinderImpl.kt
@@ -3,8 +3,10 @@ package org.intellij.sdk.action
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import java.util.Date
+import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.pow
+import kotlin.math.roundToInt
 import kotlin.time.Duration
 
 /**
@@ -42,6 +44,11 @@ data class KnowledgeState(
 
     val totalKnowledgeLevel: Double
         get() = totalKnowledge / totalLineWeight.toDouble()
+
+    fun withLines(newLines: List<LineWithKnowledge>) = KnowledgeState(
+        date = date,
+        lines = newLines,
+    )
 }
 
 /**
@@ -159,92 +166,160 @@ abstract class OblivionKnowledgeStateCalculator(
 }
 
 /**
- * An algorithm, which describes
- * how knowledge is added to the developer's [KnowledgeState],
- * when he makes a change to the file.
- *
- * It consists of taking the list of inserted lines,
- * and adding some knowledge to some lines around each inserted line.
+ * An algorithm, which calculates
+ * how knowledge is added to the lines
+ * in the developer's [KnowledgeState],
+ * when somebody makes a change to the file.
  */
-class AuthorKnowledgeAdder(
-    private val lines: MutableList<LineWithKnowledge>,
-) {
-    companion object {
-        const val SPREAD_COEFFICIENT = 6 // to one direction
-        const val READING_KNOWLEDGE_COEFFICIENT = 1
-    }
+interface LineKnowledgeAdder {
+    /**
+     * Calculates the knowledge addition
+     * based on given [developer],
+     * his knowledge [state]
+     * and [insertedLineIndices] (enumerated from 0).
+     *
+     * @return a list of reals from 0 to 1 -
+     * the amount of knowledge to be added
+     * to each line correspondingly.
+     */
+    fun calculateAddition(
+        developer: String,
+        state: KnowledgeState,
+        insertedLineIndices: List<Int>,
+    ): List<Double>
 
     /**
-     * Add knowledge for each of [insertedLineIndices].
+     * Adds the addition calculated in [calculateAddition]
+     * to given knowledge [state].
+     *
+     * @return new [KnowledgeState] with added knowledge.
      */
-    fun addKnowledge(insertedLineIndices: List<Int>) {
-        insertedLineIndices.forEach { lineIndex ->
-            addKnowledge(lineIndex)
+    fun add(
+        developer: String,
+        state: KnowledgeState,
+        insertedLineIndices: List<Int>,
+    ): KnowledgeState {
+        val addition = calculateAddition(developer, state, insertedLineIndices)
+        if (addition.size != state.lines.size) {
+            throw InternalError(
+                "Bad LineKnowledgeAdder implementation: calculateAddition() returned addition," +
+                        " the size of which (${addition.size}) does not match with the size of the file (${state.lines.size}"
+            )
         }
+        val lines = state.lines.mapIndexed { index, lineWithKnowledge ->
+            val unsafeNewKnowledge = lineWithKnowledge.knowledge + addition[index]
+            lineWithKnowledge.withKnowledge(
+                min(1.0, max(0.0, unsafeNewKnowledge))
+            )
+        }
+        return state.withLines(lines)
     }
+}
 
-    /**
-     * Add knowledge around given [lineIndex].
-     */
-    private fun addKnowledge(lineIndex: Int) {
-        // knowledge by writing --- added to this exact line
-        lines[lineIndex] = lines[lineIndex].withKnowledge(1.0)
+/**
+ * Main implementation of [LineKnowledgeAdder].
+ */
+object LineKnowledgeAdderImpl : LineKnowledgeAdder {
+    private const val SPREAD_COEFFICIENT = 6 // to one direction
+    private const val SAME_AUTHOR_WRITING_KNOWLEDGE = 1.0
+    private const val OTHER_AUTHOR_WRITING_KNOWLEDGE = 0.0
+    private const val READING_KNOWLEDGE_COEFFICIENT = 1.0
 
-        // knowledge by reading --- add to lines around it
-        val w = lines[lineIndex].line.weight
-        if (w <= 0) {
-            return // nothing to add
-        }
-        val spreadWeight = w * SPREAD_COEFFICIENT
-        for (direction in listOf(+1, -1)) {
-            spreadReadingKnowledge(lineIndex + direction, direction, spreadWeight)
-        }
-    }
+    override fun calculateAddition(
+        developer: String,
+        state: KnowledgeState,
+        insertedLineIndices: List<Int>
+    ) = AdditionBuilder(developer, state, insertedLineIndices).build()
 
-    private fun inBounds(index: Int) = index in lines.indices
-
-    /**
-     * Spreads knowledge in a given [direction] from [startingIndex].
-     */
-    private fun spreadReadingKnowledge(
-        startingIndex: Int,
-        direction: Int, // +1 or -1
-        spreadWeight: Int,
+    private class AdditionBuilder(
+        private val developer: String,
+        state: KnowledgeState,
+        private val insertedLineIndices: List<Int>,
     ) {
-        var index = startingIndex
-        var remainingWeight = spreadWeight
-        while (inBounds(index) && remainingWeight > 0) {
-            spreadReadingKnowledgeAt(index, spreadWeight, remainingWeight)
-            remainingWeight -= lines[index].line.weight
-            index += direction
-        }
-    }
+        private val lines = state.lines
+        private val size = lines.size
 
-    private fun sum(r: Int): Int = r * (r + 1) / 2
+        private val addition = MutableList(size) { 0.0 }
 
-    private fun sum(l: Int, r: Int): Int {
-        if (l > r) return 0
-        return sum(r) - sum(l - 1)
-    }
+        fun build(): List<Double> {
+            insertedLineIndices.forEach { lineIndex ->
+                addKnowledge(lineIndex)
+            }
+            return addition
+        }
 
-    private fun spreadReadingKnowledgeAt(
-        index: Int,
-        spreadWeight: Int,
-        remainingWeight: Int,
-    ) {
-        val weight = lines[index].line.weight
-        if (weight <= 0) {
-            return // nothing to add
+        private fun addAt(lineIndex: Int, value: Double) {
+            // We don't want to add something twice
+            // to the same line, so we just take
+            // the largest of additions for each line.
+            addition[lineIndex] = max(addition[lineIndex], value)
         }
-        val absoluteDelta = if (remainingWeight < weight) {
-            sum(1, remainingWeight).toDouble() / spreadWeight.toDouble()
-        } else {
-            sum(remainingWeight - weight + 1, remainingWeight).toDouble() / spreadWeight.toDouble()
+
+        private fun addKnowledge(lineIndex: Int) {
+            // knowledge by writing --- added to this exact line
+            val writingKnowledge = if (lines[lineIndex].line.author == developer) {
+                SAME_AUTHOR_WRITING_KNOWLEDGE
+            } else {
+                OTHER_AUTHOR_WRITING_KNOWLEDGE
+            }
+            addAt(lineIndex, writingKnowledge)
+
+            // knowledge by reading --- add to the lines around it
+            val w = lines[lineIndex].line.weight
+            if (w <= 0) {
+                return // nothing to add
+            }
+            val spreadWeight = (w * writingKnowledge * SPREAD_COEFFICIENT).roundToInt()
+            for (direction in listOf(+1, -1)) {
+                spreadReadingKnowledge(lineIndex + direction, direction, spreadWeight)
+            }
         }
-        val scaledAbsoluteDelta = absoluteDelta * READING_KNOWLEDGE_COEFFICIENT
-        val relativeDelta = scaledAbsoluteDelta / weight
-        val newKnowledge = min(1.0, lines[index].knowledge + relativeDelta)
-        lines[index] = lines[index].withKnowledge(newKnowledge)
+
+        /**
+         * Spreads knowledge in a given [direction] from [startingIndex].
+         */
+        private fun spreadReadingKnowledge(
+            startingIndex: Int,
+            direction: Int,
+            spreadWeight: Int,
+        ) {
+            assert(direction in listOf(+1, -1))
+            var index = startingIndex
+            var remainingWeight = spreadWeight
+            while (index in addition.indices && remainingWeight > 0) {
+                spreadReadingKnowledgeAt(index, spreadWeight, remainingWeight)
+                remainingWeight -= lines[index].line.weight
+                index += direction
+            }
+        }
+
+        // 1 + 2 + ... + r
+        private fun sum(r: Int): Int = r * (r + 1) / 2
+
+        // l + (l + 1) + ... + r
+        private fun sum(l: Int, r: Int): Int {
+            if (l > r) return 0
+            return sum(r) - sum(l - 1)
+        }
+
+        private fun spreadReadingKnowledgeAt(
+            index: Int,
+            spreadWeight: Int,
+            remainingWeight: Int,
+        ) {
+            val weight = lines[index].line.weight
+            if (weight <= 0) {
+                return // nothing to add
+            }
+            val absoluteDelta = if (remainingWeight < weight) {
+                sum(1, remainingWeight).toDouble() / spreadWeight.toDouble()
+            } else {
+                sum(remainingWeight - weight + 1, remainingWeight).toDouble() / spreadWeight.toDouble()
+            }
+            val scaledAbsoluteDelta = absoluteDelta * READING_KNOWLEDGE_COEFFICIENT
+            val relativeDelta = scaledAbsoluteDelta / weight
+            addAt(index, relativeDelta)
+        }
     }
 }
 
@@ -252,13 +327,11 @@ class AuthorKnowledgeAdder(
  * Main implementation of [KnowledgeStateCalculator].
  *
  * Works as an [OblivionKnowledgeStateCalculator] with given [oblivionFunction].
- * A commit change is handled this way:
- * * if a commit is done by the same developer, add knowledge via [AuthorKnowledgeAdder];
- * * if a commit is done by another developer, knowledge of untouched lines does not change,
- * and knowledge of added lines is zero.
+ * A commit change is handled by [adder].
  */
 class MainKnowledgeStateCalculatorImpl(
     oblivionFunction: OblivionFunction = MainOblivionFunctionImpl,
+    private val adder: LineKnowledgeAdder = LineKnowledgeAdderImpl,
 ) : OblivionKnowledgeStateCalculator(oblivionFunction) {
 
     override fun instantNextKnowledgeState(
@@ -274,16 +347,14 @@ class MainKnowledgeStateCalculatorImpl(
             }
             override fun onInsertedLine(line2: Int) {
                 val line = nextRevision.content[line2]
-                lines.add(LineWithKnowledge(line, 0.0))
+                lines.add(LineWithKnowledge(line, 0.0)) // initially knowledge is zero on an inserted line
                 insertedLineIndices.add(lines.size - 1)
             }
         }
         DiffExecutor(handler).execute(upToDateState.lines.size, nextRevision.differenceWithPrevious)
-        if (nextRevision.author == developer) {
-            val adder = AuthorKnowledgeAdder(lines)
-            adder.addKnowledge(insertedLineIndices)
-        }
-        return KnowledgeState(nextRevision.date, lines)
+
+        val stateWithInsertedLines = upToDateState.withLines(lines)
+        return adder.add(developer, stateWithInsertedLines, insertedLineIndices)
     }
 }
 
